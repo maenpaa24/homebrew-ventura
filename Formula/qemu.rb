c2a429eb682d5fa112485aa7ac9f92c31bfb786d
maenpaa24 <maenpaa24@gmail.com>
2026-09-25 14:07:14 +0200

    add qemu

---
+++ Formula/qemu.rb
@@ -0,0 +1,138 @@
+class Qemu < Formula
+  desc "Generic machine emulator and virtualizer"
+  homepage "https://www.qemu.org/"
+  url "https://download.qemu.org/qemu-10.1.0.tar.xz"
+  sha256 "e0517349b50ca73ebec2fa85b06050d5c463ca65c738833bd8fc1f15f180be51"
+  license "GPL-2.0-only"
+  head "https://gitlab.com/qemu-project/qemu.git", branch: "master"
+
+  livecheck do
+    url "https://www.qemu.org/download/"
+    regex(/href=.*?qemu[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  bottle do
+    sha256 arm64_sequoia: "de0820b108623339ac1d2c9d151b94c8348e79b15d86de4246c87bfd81f2629d"
+    sha256 arm64_sonoma:  "34e4bfc4ac32d15353d23dfa1b102824c951af7bc140a4ccba199e4c4f8e7964"
+    sha256 arm64_ventura: "65a91d10b70eac0405945d75d1155e0efc5fa0348301ef1d33566c442bf45ac1"
+    sha256 sonoma:        "bf5430315856fc8f584c778a47381579a9c78da395ead5097b956d234da2ec2d"
+    sha256 ventura:       "169adf5d3b18aa6198ab1794d95279fbaca57ead0de451ddead37a11cfc4bf01"
+    sha256 arm64_linux:   "fa1c2e3cd6052522107dd01369a101d7148bd1978506b6b5f4f0f2b8fc642873"
+    sha256 x86_64_linux:  "97637f18f83b40f27cb19e2f9d43a3c95c68d6cd0b413cd1a3ac6b269b71ed1b"
+  end
+
+  depends_on "libtool" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkgconf" => :build
+  depends_on "python@3.13" => :build # keep aligned with meson
+  depends_on "spice-protocol" => :build
+
+  depends_on "capstone"
+  depends_on "dtc"
+  depends_on "glib"
+  depends_on "gnutls"
+  depends_on "jpeg-turbo"
+  depends_on "libpng"
+  depends_on "libslirp"
+  depends_on "libssh"
+  depends_on "libusb"
+  depends_on "lzo"
+  depends_on "ncurses"
+  depends_on "nettle"
+  depends_on "pixman"
+  depends_on "snappy"
+  depends_on "vde"
+  depends_on "zstd"
+
+  uses_from_macos "bison" => :build
+  uses_from_macos "flex" => :build
+  uses_from_macos "bzip2"
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "attr"
+    depends_on "cairo"
+    depends_on "elfutils"
+    depends_on "gdk-pixbuf"
+    depends_on "gtk+3"
+    depends_on "libcap-ng"
+    depends_on "libepoxy"
+    depends_on "libx11"
+    depends_on "libxkbcommon"
+    depends_on "mesa"
+    depends_on "systemd"
+  end
+
+  def install
+    ENV["LIBTOOL"] = "glibtool"
+
+    # Remove wheels unless explicitly permitted. Currently this:
+    # * removes `meson` so that brew `meson` is always used
+    # * keeps `pycotap` which is a pure-python "none-any" wheel (allowed in homebrew/core)
+    rm(Dir["python/wheels/*"] - Dir["python/wheels/pycotap-*-none-any.whl"])
+
+    args = %W[
+      --prefix=#{prefix}
+      --cc=#{ENV.cc}
+      --host-cc=#{ENV.cc}
+      --disable-bsd-user
+      --disable-download
+      --disable-guest-agent
+      --enable-slirp
+      --enable-capstone
+      --enable-curses
+      --enable-fdt=system
+      --enable-libssh
+      --enable-vde
+      --enable-virtfs
+      --enable-zstd
+      --extra-cflags=-DNCURSES_WIDECHAR=1
+      --disable-sdl
+    ]
+
+    # Sharing Samba directories in QEMU requires the samba.org smbd which is
+    # incompatible with the macOS-provided version. This will lead to
+    # silent runtime failures, so we set it to a Homebrew path in order to
+    # obtain sensible runtime errors. This will also be compatible with
+    # Samba installations from external taps.
+    args << "--smbd=#{HOMEBREW_PREFIX}/sbin/samba-dot-org-smbd"
+
+    args += if OS.mac?
+      ["--disable-gtk", "--enable-cocoa"]
+    else
+      ["--enable-gtk"]
+    end
+
+    system "./configure", *args
+    system "make", "V=1", "install"
+  end
+
+  test do
+    # 820KB floppy disk image file of FreeDOS 1.2, used to test QEMU
+    # NOTE: Keep outside test block so that `brew fetch` is able to handle slow download/retries
+    resource "homebrew-test-image" do
+      url "https://www.ibiblio.org/pub/micro/pc-stuff/freedos/files/distributions/1.2/official/FD12FLOPPY.zip"
+      sha256 "81237c7b42dc0ffc8b32a2f5734e3480a3f9a470c50c14a9c4576a2561a35807"
+    end
+
+    archs = %w[
+      aarch64 alpha arm avr hppa i386 loongarch64 m68k microblaze microblazeel mips
+      mips64 mips64el mipsel or1k ppc ppc64 riscv32 riscv64 rx
+      s390x sh4 sh4eb sparc sparc64 tricore x86_64 xtensa xtensaeb
+    ]
+    archs.each do |guest_arch|
+      assert_match version.to_s, shell_output("#{bin}/qemu-system-#{guest_arch} --version")
+    end
+
+    resource("homebrew-test-image").stage testpath
+    assert_match "file format: raw", shell_output("#{bin}/qemu-img info FLOPPY.img")
+
+    # On macOS, verify that we haven't clobbered the signature on the qemu-system-x86_64 binary
+    if OS.mac?
+      output = shell_output("codesign --verify --verbose #{bin}/qemu-system-x86_64 2>&1")
+      assert_match "valid on disk", output
+      assert_match "satisfies its Designated Requirement", output
+    end
+  end
+end
